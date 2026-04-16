f13e88de1049f1cbb18fb3df2f67b291718ebb76
Felipe <mrcsepulveda2011@gmail.com>
2026-04-16 00:34:37 -0400

    Cargar cuentas y movimientos reales en Render

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 python manage.py migrate
-
 python manage.py flush --no-input
 
 python manage.py loaddata conceptos_ok.json
+python manage.py loaddata cuentas_ok.json
+python manage.py loaddata movimientos_ok.json
 python manage.py loaddata proyecciones_ok.json
