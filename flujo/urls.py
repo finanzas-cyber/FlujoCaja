b45fc65837d0deaef7de72df49470ab0fa820734
Felipe <mrcsepulveda2011@gmail.com>
2026-04-17 11:59:41 -0400

    Agregar url guardar movimiento real

--- flujo/urls.py
+++ flujo/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("", views.inicio, name="inicio"),
     path("actualizar-softland/", views.actualizar_softland, name="actualizar_softland"),
     path("guardar-proyeccion/", views.guardar_proyeccion, name="guardar_proyeccion"),
+    path("guardar-movimiento-real/", views.guardar_movimiento_real, name="guardar_movimiento_real"),
     path("generar-proyecciones-json/", views.generar_proyecciones_json, name="generar_proyecciones_json"),
     path("publicar-proyecciones/", views.publicar_proyecciones, name="publicar_proyecciones"),
     path("detalle-movimientos-real/", views.detalle_movimientos_real, name="detalle_movimientos_real"),
