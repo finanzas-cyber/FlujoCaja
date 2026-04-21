a3f206386bb6e9bf04e25f2687d7b905c1b81cfa
Felipe <mrcsepulveda2011@gmail.com>
2026-04-20 21:31:05 -0400

    Agregar carga de conceptos antes de proyecciones

--- build.sh
+++ build.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-# Instalar dependencias
 pip install -r requirements.txt
 
-# Recolectar archivos estáticos
 python manage.py collectstatic --no-input
-
-# Correr migraciones (por si hay cambios en las tablas)
 python manage.py migrate
 
-# EJECUTAR TU NUEVO COMANDO AUTOMÁTICO
-# Esto borrará la basura y cargará el JSON limpio
+# 🔥 CARGAR CONCEPTOS PRIMERO
+python manage.py cargar_maestros
+
+# 🔥 DESPUÉS PROYECCIONES
 python manage.py cargar_datos
