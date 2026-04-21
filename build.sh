92b8b107e094410e00561f817be5d9ae7fe7d92c
Felipe <mrcsepulveda2011@gmail.com>
2026-04-21 10:37:11 -0400

    Agregar carga de movimientos en build

--- build.sh
+++ build.sh
@@ -7,4 +7,5 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 python manage.py loaddata conceptos.json
+python manage.py loaddata movimientos_2025_2026.json
 python manage.py cargar_datos
